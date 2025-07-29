f31543e43d652a96436d0125249f6d145f98212f
danyulgh <daniel.g.hoang2007@gmail.com>
2025-07-29 16:20:29 -0400

    algorithm done w/ most freq + spread

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from satinstance import SATInstance 
 import random
+import time 
 import os
-
 def print_assignment(z):
     for i, bool in enumerate(z):
         if bool == None: continue
         print(f"Literal {i+1} set to {bool}")
 
 def randomize_assignment(assignment):
-    #random number of literals assigned
     for i in range(random.randint(0, len(assignment)-1)):
         literal = random.randint(0, len(assignment)-1)
         while assignment[literal] != None:
@@ -16,44 +15,15 @@ def randomize_assignment(assignment):
         assignment[literal] = random.choice([True, False])
     return assignment
 
-# check testing w/ random assignments
-# with open(f"tests/uf20-91/{random.choice(os.listdir("tests/uf20-91"))}", "r") as file:
-#     instance = SATInstance.instance_from_file(SATInstance, file)
-#     print("original instance:")
-#     print(instance)
-#     print()
-
-#     z = instance.assignment.copy()
-#     randomize_assignment(z)
-
-#     print(f"check result: {instance.check(z)}")
-#     print()
-
-#     print(instance)
-
-#two sat testing
-# with open(f"tests/twosats/{random.choice(os.listdir("tests/twosats"))}", "r") as file:
-#     instance = SATInstance.instance_from_file(SATInstance, file)
-#     print("original instance:")
-#     print(instance)
-#     print()
-
-#     print(f"twosat results: {instance.twosat()}")
-#     print(instance)
-
-# stand testing w/ random assignments
-with open(f"tests/uf20-91/{random.choice(os.listdir("tests/uf20-91"))}", "r") as file:
-    instance = SATInstance.instance_from_file(SATInstance, file)
-    print(f"original instance from {file.name}:")
-    print(instance)
-    print()
-
-    z = instance.assignment.copy()
-    randomize_assignment(z)
-    print_assignment(z)
-    print()
-
-    print(f"stand result: {instance.stand(z)}")
-    print()
-
-    print(instance)
+directories = ["tests/uf20-91", "tests/uf50-218", "tests/uuf50-218/UUF50.218.1000"]
+picking = [SATInstance.most_frequent, SATInstance.most_spread]
+for i in range(30):
+    directory = random.choice(directories)
+    pick = random.choice(picking)
+    with open(f"{directory}/{random.choice(os.listdir(directory))}", "r") as file:
+        instance = SATInstance.instance_from_file(SATInstance, file)
+        assignment = [None] * instance.count
+        start_time = time.perf_counter()
+        value = instance.solve(assignment, pick)
+        end_time = time.perf_counter()
+        print(f"{file.name}: {end_time-start_time} seconds for {"SAT" if value else "UNSAT"} via {pick.__name__}")
