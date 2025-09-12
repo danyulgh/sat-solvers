be879134a11217e00ddeb6d37d3902736bb6ee31
dhoang <daniel.g.hoang2007@gmail.com>
2025-09-12 14:23:13 -0400

    variable numbers no longer need to be in order

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import random
 import time 
 import os
 
-def print_assignment(z):
-    for i, bool in enumerate(z):
-        if bool == None: continue
-        print(f"Literal {i+1} set to {bool}")
+# def print_assignment(z):
+#     for i, bool in enumerate(z):
+#         if bool == None: continue
+#         print(f"Literal {i+1} set to {bool}")
 
-def randomize_assignment(assignment):
-    for i in range(random.randint(0, len(assignment)-1)):
-        literal = random.randint(0, len(assignment)-1)
-        while assignment[literal] != None:
-            literal = random.randint(1, len(assignment)-1)
-        assignment[literal] = random.choice([True, False])
-    return assignment
+# def randomize_assignment(assignment):
+#     for i in range(random.randint(0, len(assignment)-1)):
+#         literal = random.randint(0, len(assignment)-1)
+#         while assignment[literal] != None:
+#             literal = random.randint(1, len(assignment)-1)
+#         assignment[literal] = random.choice([True, False])
+#     return assignment
 
-directories = ["uf20-91", "uf50-218", "uuf50-218/UUF50.218.1000", "flat50-115", "pigeon-hole"]
+# directories = ["uf20-91", "uf50-218", "uuf50-218/UUF50.218.1000", "flat50-115"]
+directories = ["pigeon-hole"]
 funcs  = [SATInstance.most_frequent, SATInstance.most_spread]
 for i in range(30):
     directory = random.choice(directories)
@@ -26,7 +27,7 @@ for i in range(30):
     with open(f"tests/{directory}/{test}", "r") as file:
         print(f"picking via {test} with {func.__name__}")
         instance = SATInstance.instance_from_file(SATInstance, file)
-        assignment = [None] * instance.count
+        assignment = [None] * len(instance.variables)
         start_time = time.perf_counter()
         value = instance.solve(assignment, func)
         end_time = time.perf_counter()
